f30ad5b7f4221c964f543852e6b7d15171629beb
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-20 00:41:23 +0530

    restructuring

---
+++ 1. Introduction/1. starting.rb
@@ -0,0 +1,6 @@
+
+# printing onto the screen
+print "Hello World";    #prints with no line break at the end
+puts "Hello";           # prints with a new line at the end
+
+# shortcut to run is ctrl + alt + n
--- index.rb
+++
@@ -1,6 +0,0 @@
-
-# printing onto the screen
-print "Hello World";
-puts "Hello";
-
-# shortcut to run is ctrl + alt + n
