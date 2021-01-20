2ac4c61c0dcd2df405512e2ccb7fb1c6d51ebae8
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-20 17:51:31 +0530

    update

--- 1. Introduction/1. starting.rb
+++ 1. Introduction/1. starting.rb
@@ -1,6 +1,34 @@
+# shortcut to run is ctrl + alt + n
 
 # printing onto the screen
-print "Hello World";    #prints with no line break at the end
-puts "Hello";           # prints with a new line at the end
+print ("Hello World");    #prints with no line break at the end
+puts ("Hello")            # prints with a new line at the end
+print "Hello World \n"  
 
-# shortcut to run is ctrl + alt + n
+# no semicolons and parantheses needed in every case with the exception of some special cases, but just put them anyway
+
+# variables
+
+# null is nil here
+thisIsTheNullValue = nil;
+
+# booleans start with simple t and f
+
+# no explicitly typed data types
+name = "potato";
+puts "Monkey, #{name}";     # same kind of insertion as in js
+puts "Monkey, " + name;
+
+# shit with strings
+
+name = "potato";
+
+# the parantheses for calling this function are apparently optional but just type them anyway
+puts phrase.upcase();
+puts phrase.downcase();
+
+# others are .strip(), .length()
+
+phrase = "Testing testing testing still testing";
+
+puts (phrase.include?"testing")
